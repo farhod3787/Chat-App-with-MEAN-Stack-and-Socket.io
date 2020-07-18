4195a3223b95ae820a37c991ccb49050b53f1b5b
farhod3787 <farhod.marufjonov@mail.ru>
2020-07-18 21:19:20 +0500

    added cloud url

--- backend/app.js
+++ backend/app.js
@@ -11,32 +11,19 @@ const app = express();
 app.use(cors());
 
 
-// mongoose.connect( config.CLOUD_URL, { useNewUrlParser: true })
-//     .then(() => {
-//         console.log('Cloud MongoDb connected.');
-//     })
-//     .catch(err => console.log(err));
-
-mongoose.connect(
-  config.MONGO_URL
-  ).then( () => {
-    console.log('Connected to local database')
-})
-.catch( () =>{
-    console.log('Error in connected database')
-});
-
+mongoose.connect( config.CLOUD_URL, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Cloud MongoDb connected.');
+    })
+    .catch(err => console.log(err));
 
 
 module.exports = { mongoose };
 
-
-// app.use(express.bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/images', express.static(path.join("backend/images")));
-
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -52,6 +39,5 @@ app.use((req, res, next) => {
 });
 
 app.use('/api', routes.api);
-
 
 module.exports = app;
